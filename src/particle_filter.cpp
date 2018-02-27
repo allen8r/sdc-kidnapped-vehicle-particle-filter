2b1e28dc14c5a9a243d853915f08dd9d47407024
Allen C. Lau <allen.c.lau@gmail.com>
2018-02-27 12:34:56 -0800

    Implement ParticleFilter::init(), ParticleFilter::prediction(), and ParticleFilter::dataAssociation()

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -2,7 +2,7 @@
  * particle_filter.cpp
  *
  *  Created on: Dec 12, 2016
- *      Author: Tiffany Huang
+ *      Author: Tiffany Huang, Allen Lau
  */
 
 #include <random>
@@ -24,6 +24,41 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
+	num_particles = 20;
+
+	// consider checking for null array and array size to prevent runtime array index errors
+	double std_x = std[0];
+	double std_y = std[1];
+	double std_theta = std[2];
+	
+	default_random_engine rand_gen;
+
+	// Normal Gaussian distributions for x, y, and theta
+	normal_distribution<double> dist_x(x, std_x);
+	normal_distribution<double> dist_y(y, std_y);
+	normal_distribution<double> dist_theta(theta, std_theta);
+
+	// Create particles using the Gaussians for sampling - introducing noise
+	for (int i = 0; i < num_particles; i++) {
+		double sample_x = dist_x(rand_gen);
+		double sample_y = dist_y(rand_gen);
+		double sample_theta = dist_theta(rand_gen);
+
+		Particle p = Particle();
+		p.id = i;
+		p.x = sample_x;
+		p.y = sample_y;
+		p.theta = sample_theta;
+		p.weight = 1;
+
+		particles.push_back(p);
+	}
+
+	if (num_particles != particles.size()) {
+		std::cout << "Error: Number of particles does not match num_particles! Check ParticleFilter::init()" 
+			<< std::endl;
+	}
+
 
 }
 
@@ -33,6 +68,38 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
+	if (fabs(yaw_rate) < 0.0001) {
+		std::cout << "Error: division by zero, bad yaw_rate provided to ParticleFilter::prediction()!"
+			<< std::endl;
+		return;
+	}
+
+	double v_over_yaw_rate = velocity / yaw_rate;
+
+	double std_x = std_pos[0];
+	double std_y = std_pos[1];
+	double std_theta = std_pos[2];
+
+	default_random_engine rand_gen;
+	
+	for (int i = 0; i < num_particles; i++) {
+		Particle p = particles[i];
+		p.x = p.x + v_over_yaw_rate * (sin(p.theta + yaw_rate * delta_t) - sin(p.theta));
+		p.y = p.y + v_over_yaw_rate * (cos(p.theta) - cos(p.theta + yaw_rate * delta_t));
+		p.theta = p.theta + yaw_rate * delta_t;
+
+		// Add Gaussian noise
+		// Normal Gaussian distributions for x, y, and theta
+		normal_distribution<double> dist_x(p.x, std_x);
+		normal_distribution<double> dist_y(p.y, std_y);
+		normal_distribution<double> dist_theta(p.theta, std_theta);
+		
+		p.x = dist_x(rand_gen);
+		p.y = dist_y(rand_gen);
+		p.theta = dist_theta(rand_gen);
+
+	}
+
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
@@ -40,7 +107,18 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
-
+	for (int i = 0; i < observations.size(); i++) {
+		double minDist = std::numeric_limits<double>::max();
+		int bestIndex = 0;
+		for (int p = 0; p < predicted.size(); p++) {
+			double d = dist(predicted[p].x, predicted[p].y, observations[i].x, observations[i].y);
+			if (d < minDist) {
+				minDist = d;
+				bestIndex = p;
+			}
+		}
+		predicted[bestIndex].id = observations[i].id;
+	}
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
